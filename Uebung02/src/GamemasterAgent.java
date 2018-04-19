a8bef300acefe14dbcb7f3a56f5d758b21322993
marcelldeluxee <marcelldeluxee@outlook.de>
2018-04-19 10:53:40 +0200

    anmeldung und kartendeck
    anmeldung und kartendeck, danach kommt karten austeilen und spielbegin

--- Uebung02/src/GamemasterAgent.java
+++ Uebung02/src/GamemasterAgent.java
@@ -1,5 +1,90 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class GamemasterAgent extends Agent {
+
+	private ArrayList<AID> players;
+	private ArrayList<String> deck;
+	
+	
+	@Override
+	protected void setup() {
+		players = new ArrayList<AID>();
+		deck = new ArrayList<String>();
+		initDeck();
+		
+		addBehaviour(new CyclicBehaviour(this) {	
+		
+		/**
+		 * Receive and response the registration-messages from players to registration in an game.
+		 * 
+		 */
+		@Override
+		public void action() {
+			ACLMessage msg = receive();
+			if (msg != null) {
+				String msgContent = msg.getContent();
+				AID msgSender = msg.getSender();
+				ACLMessage msgResponse = new ACLMessage(ACLMessage.INFORM);
+		
+				if (msgContent.contains("rigistration")) {
+					
+				
+					players.add(msgSender);
+					System.out.println(msgSender.getLocalName() + " is in game");
+			}
+			send(msgResponse);
+		} else {
+			block();
+		}
+		}
+
+		});
+	}
+	
+	private void initDeck() {
+		deck.add("K7");
+		deck.add("K8");
+		deck.add("K9");
+		deck.add("KZ");
+		deck.add("KB");
+		deck.add("KD");
+		deck.add("KK");
+		deck.add("KA");
+		
+		deck.add("P7");
+		deck.add("P8");
+		deck.add("P9");
+		deck.add("PZ");
+		deck.add("PB");
+		deck.add("PD");
+		deck.add("PK");
+		deck.add("PA");
+		
+		deck.add("H7");
+		deck.add("H8");
+		deck.add("H9");
+		deck.add("HZ");
+		deck.add("HB");
+		deck.add("HD");
+		deck.add("HK");
+		deck.add("HA");
+		
+		deck.add("C7");
+		deck.add("C8");
+		deck.add("C9");
+		deck.add("CZ");
+		deck.add("CB");
+		deck.add("CD");
+		deck.add("CK");
+		deck.add("CA");
+		
+		Collections.shuffle(deck);
+	}
 
 }
--- Uebung02/src/PlayerAgent.java
+++ Uebung02/src/PlayerAgent.java
@@ -1,5 +1,37 @@
+import java.util.HashMap;
+import java.util.Random;
+
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class PlayerAgent extends Agent {
 
+	private AID gamemasterAgent;
+	
+	
+	@Override
+	protected void setup() {
+		gamemasterAgent = new AID("master", AID.ISLOCALNAME);
+		RegistrationRequest registrationRequest = new RegistrationRequest();
+		registrationRequest.action();
+		
+		
+		
+	}
+		
+		/**
+		 * the request for registration in game to gamemaster
+		 * 
+		 */
+		private class RegistrationRequest extends OneShotBehaviour {		
+			@Override
+			public void action() {
+				ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+				msg.addReceiver(gamemasterAgent);
+				msg.setContent("rigistration");
+				send(msg);
+			}
+		}
 }
