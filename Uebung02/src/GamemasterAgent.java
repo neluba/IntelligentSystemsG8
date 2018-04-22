9c8e1c27950befb91767fb0743b10b97f8386de0
marcelldeluxee <marcelldeluxee@outlook.de>
2018-04-22 21:26:18 +0200

    uebung 2 final
    uebung 2 final

--- Uebung02/src/GamemasterAgent.java
+++ Uebung02/src/GamemasterAgent.java
@@ -6,47 +6,165 @@ import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 
+/**
+ * the agent to admin the game
+ * 
+ * @author IS Group 08
+ *
+ */
 public class GamemasterAgent extends Agent {
 
 	private ArrayList<AID> players;
+	private int playersInGame;
+	private int currentPlayerIndex;
 	private ArrayList<String> deck;
+	private String tableCard;
+	private ArrayList<String> tableCardStack;
 	
 	
 	@Override
 	protected void setup() {
 		players = new ArrayList<AID>();
+		currentPlayerIndex = 0;
+		playersInGame = 0;
 		deck = new ArrayList<String>();
+		tableCardStack = new ArrayList<String>();
 		initDeck();
 		
 		addBehaviour(new CyclicBehaviour(this) {	
 		
 		/**
 		 * Receive and response the registration-messages from players to registration in an game.
+		 * Receive and response the table card and the cards that the players played. 
 		 * 
 		 */
 		@Override
 		public void action() {
 			ACLMessage msg = receive();
 			if (msg != null) {
-				String msgContent = msg.getContent();
-				AID msgSender = msg.getSender();
 				ACLMessage msgResponse = new ACLMessage(ACLMessage.INFORM);
-		
-				if (msgContent.contains("rigistration")) {
+				String inform = "";
+				AID msgSender = msg.getSender();
+				String msgContent = msg.getContent();
+				String[] msgContentAsString = msgContent.split("\n");
+				String msgHead = msgContentAsString[0];
+				switch (msgHead) {		
 					
-				
+				case "rigistration":  
+					inform = "playerCards\n";
+					int playerCardsCount = 5;
+					for (int i = 0; i < playerCardsCount; i++) {
+						String card = deck.get(0);
+						deck.remove(0);
+						inform += card + "\n";
+					}
+					msgResponse.setContent(inform);
+					msgResponse.addReceiver(msgSender);
 					players.add(msgSender);
-					System.out.println(msgSender.getLocalName() + " is in game");
+					playersInGame++;
+					send(msgResponse);
+					if (playersInGame == 3) {
+						Collections.sort(players);
+						tableCard = deck.get(0);
+						tableCardStack.add(tableCard);
+						deck.remove(0);
+						playCard();
+					}
+					break;
+				
+				// player can not play a card	
+				case "pass":
+					takeCard();
+					currentPlayerIndex++;
+					if (currentPlayerIndex >= players.size())
+						currentPlayerIndex = 0;
+					playCard();
+					break;
+				
+				// player played a card	
+				case "playedCard":
+					String colorTableCard = tableCard.substring(0, 1);
+					String valueTableCard = tableCard.substring(1);
+					String playedCard = msgContentAsString[1];
+					String colorPlayedCard = playedCard.substring(0, 1);
+					String valuePlayedCard = playedCard.substring(1);
+					String playersHandSize = msgContentAsString[2];
+					int playerHandSizeAsInt = Integer.parseInt(playersHandSize);
+					System.out.println("Playercard: " + playedCard);
+					System.out.println("Playerhandsize: " + playersHandSize);
+					if (colorTableCard.contains(colorPlayedCard) || valueTableCard.contains(valuePlayedCard)) {
+						if (playerHandSizeAsInt < 1) {
+							System.out.println("Game Over! The winner is: " + msgSender.getLocalName());
+							msgResponse.setContent("gameOver");
+							for (AID player : players) {
+								msgResponse.addReceiver(player);
+							}
+							send(msgResponse);
+							doDelete();
+						}
+						tableCard = playedCard;
+						tableCardStack.add(tableCard);
+						currentPlayerIndex++;
+						if (currentPlayerIndex >= players.size())
+							currentPlayerIndex = 0;
+						playCard();
+					// player card is not valid
+					} else {
+						takeCard();
+						playCard();
+					}
+					break;
+				}
+			} else {
+				block();
 			}
-			send(msgResponse);
-		} else {
-			block();
 		}
-		}
-
 		});
 	}
 	
+	/**
+	 * Inform the players about the actually tablecard and instruct the next player to play a card.
+	 */
+	private void playCard() {
+		ACLMessage msgRequestTableCard = new ACLMessage(ACLMessage.INFORM);
+		String inform = "";
+		inform = "tableCard\n" + tableCard;
+		msgRequestTableCard.setContent(inform);
+		for (AID player : players) {
+			msgRequestTableCard.addReceiver(player);
+		}
+		send(msgRequestTableCard);
+		
+		ACLMessage msgRequestPlayCard = new ACLMessage(ACLMessage.INFORM);
+		inform = "playCard";
+		msgRequestPlayCard.setContent(inform);
+		AID currentPlayer = players.get(currentPlayerIndex);
+		msgRequestPlayCard.addReceiver(currentPlayer);
+		send(msgRequestPlayCard);
+	}
+	
+	/**
+	 * Instruct a player to get a card from cardstack.
+	 */
+	private void takeCard() {
+		ACLMessage msgResponse = new ACLMessage(ACLMessage.INFORM);
+		String inform = "getCard\n";
+		if (deck.isEmpty()) {
+			Collections.shuffle(tableCardStack);
+			deck.addAll(tableCardStack);
+		}
+		String card = deck.get(0);
+		deck.remove(0);
+		inform += card; 
+		msgResponse.setContent(inform);
+		AID currentPlayer = players.get(currentPlayerIndex);
+		msgResponse.addReceiver(currentPlayer);
+		send(msgResponse);
+	}
+	
+	/**
+	 * initialize the carddeck
+	 */
 	private void initDeck() {
 		deck.add("K7");
 		deck.add("K8");
@@ -86,5 +204,6 @@ public class GamemasterAgent extends Agent {
 		
 		Collections.shuffle(deck);
 	}
+	
 
 }
--- Uebung02/src/PlayerAgent.java
+++ Uebung02/src/PlayerAgent.java
@@ -1,37 +1,120 @@
-import java.util.HashMap;
-import java.util.Random;
-
+import java.util.ArrayList;
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 
+/**
+ * the agent who play in game
+ * 
+ * @author IS Group 08
+ *
+ */
 public class PlayerAgent extends Agent {
 
-	private AID gamemasterAgent;
+	private AID gamemaster;
+	private ArrayList<String> playerHand;
+	private String tableCard;
 	
 	
 	@Override
 	protected void setup() {
-		gamemasterAgent = new AID("master", AID.ISLOCALNAME);
+		gamemaster = new AID("Gamemaster", AID.ISLOCALNAME);
+		playerHand = new ArrayList<String>();		
 		RegistrationRequest registrationRequest = new RegistrationRequest();
 		registrationRequest.action();
 		
-		
-		
-	}
-		
-		/**
-		 * the request for registration in game to gamemaster
-		 * 
-		 */
-		private class RegistrationRequest extends OneShotBehaviour {		
+		addBehaviour(new CyclicBehaviour(this) {			
+			
+			/**
+			 * Receive and response the messages from and to gameadmin to play the game.
+			 * Plot the conversations between agents in console. 	
+			 *  
+			 */
 			@Override
 			public void action() {
-				ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-				msg.addReceiver(gamemasterAgent);
-				msg.setContent("rigistration");
-				send(msg);
+				ACLMessage msg = receive();
+				if (msg != null) {
+					String msgContent = msg.getContent();
+					String[] msgContentAsString = msgContent.split("\n");
+					String msgHead = msgContentAsString[0];
+					switch (msgHead) {		
+					
+					case "playerCards":					
+						for (int i = 0; i < msgContentAsString.length; i++) {
+							playerHand.add(msgContentAsString[i]);
+						}
+						break;
+						
+					case "tableCard":
+						tableCard = msgContentAsString[1];
+						break;					
+					
+					case "playCard":
+						if (!playerHand.isEmpty()) {
+							System.out.println();	
+							System.out.println(this.getAgent().getLocalName() + " is turning");
+							System.out.println("Tablecard: " + tableCard);
+							ACLMessage msgRequest = new ACLMessage(ACLMessage.INFORM);
+							String inform = "";
+							boolean pass = true;
+							String colorTableCard = tableCard.substring(0, 1);
+							String valueTableCard = tableCard.substring(1);
+							int playerHandIndex = 0;
+							for (String card : playerHand) {
+								String color = card.substring(0, 1);
+								String value = card.substring(1);
+								if (color.contains(colorTableCard) || value.contains(valueTableCard)) {
+									pass = false;
+									inform = "playedCard\n" + card;
+									playerHand.remove(playerHandIndex);
+									inform += "\n" + (playerHand.size() - 1);
+									msgRequest.setContent(inform);
+									msgRequest.addReceiver(gamemaster);
+									break;
+								}
+								playerHandIndex++;
+							}
+							if (pass == true) {
+								inform = "pass";
+								msgRequest.setContent(inform);
+								System.out.println(msgRequest.getContent());
+								System.out.println("Playerhandsize: " + playerHand.size());
+								msgRequest.addReceiver(gamemaster);
+							}
+							send(msgRequest);
+						}
+						break;
+						
+					case "getCard":
+						String card = msgContentAsString[1];
+						playerHand.add(card);
+						break;
+									
+					case "gameOver":
+						doDelete();
+					}
+											
+				} else {
+					block();
+				}
 			}
+		});
+	}
+		
+	/**
+	 * the request for registration in game to gameadmin
+	 * 
+	 */
+	private class RegistrationRequest extends OneShotBehaviour {		
+		@Override
+		public void action() {
+			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+			msg.addReceiver(gamemaster);
+			msg.setContent("rigistration");
+			send(msg);
 		}
+	}
+
 }
